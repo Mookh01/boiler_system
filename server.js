d5a65801dcd9a213ae74bc639fd779304444cab9
keith <k1a2m77@gmail.com>
2017-02-13 13:00:52 -0600

    home adjustment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/home', function(req, res) {
+app.get('/', function(req, res) {
     res.render('home');
 });
 
